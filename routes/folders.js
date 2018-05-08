b8dc22200b0d6395f8b1b5d5ba3e32cda90a2ef9
Tammy Tian <tiantammy@outlook.com>
2018-05-08 20:57:50 +0200

    protected api enpoints with jwtpassport

--- routes/folders.js
+++ routes/folders.js
@@ -2,6 +2,9 @@
 
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+router.use('/', passport.authenticate('jwt', { session: false, failWithError: true }));
+
 
 const mongoose = require('mongoose');
 
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@
 
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+router.use('/', passport.authenticate('jwt', { session: false, failWithError: true }));
 
 //const mongoose = require('mongoose');
 
